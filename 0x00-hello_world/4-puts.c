5e08b94da01e67ea58522ebf02dea1c1425f229c
Salztech1 <salomenjinda0@gmail.com>
2023-08-10 14:51:23 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program thet prins a line with put fuction
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
