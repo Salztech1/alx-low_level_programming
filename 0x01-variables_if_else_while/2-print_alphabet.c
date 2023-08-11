51861c97167d47c08b6c48a4d7dd9f9b61f55f6e
Salztech1 <salomenjinda0@gmail.com>
2023-08-11 16:52:18 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,13 @@
  */
 int main(void)
 {
-	char c;
+	char d;
 
-	c = 'a';
+	d = 'a';
 	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
+		(d <= 'z') {
+			putchar(d);
+			d++;
 		}
 	putchar('\n');
 	return (0);
