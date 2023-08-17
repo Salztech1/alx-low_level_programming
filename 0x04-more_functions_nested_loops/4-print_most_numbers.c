b228b310347322387afda60a906d2b5b0d972d60
Salztech1 <salomenjinda0@gmail.com>
2023-08-17 08:58:36 -0700

    4-print_most_numbers.c clear
    clear
    clear
    
    clear
    clear
    git commit -m 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - A function that prints numbers from (0 to 9) without (2 and 4)
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
+	}
+	_putchar('\n');
+}
