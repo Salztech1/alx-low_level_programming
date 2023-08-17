31f001562ae46ed87b63c3094d981f9af2ddbfe7
Salztech1 <salomenjinda0@gmail.com>
2023-08-17 07:46:17 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - A function that multiplies two digits
+ * @a: variable
+ * @b: variable
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int mull;
+
+	mull = a * b;
+	return (mull);
+}
